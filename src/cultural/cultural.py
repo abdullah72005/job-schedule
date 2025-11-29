84e7f81a3621ae1440d12ff4090d7ba230869529
marwan-salem18 <166882779+marwan-salem18@users.noreply.github.com>
2025-11-29 16:35:07 +0200

    Cultural finished (hopefully)

--- src/cultural/cultural.py
+++ src/cultural/cultural.py
@@ -5,83 +5,90 @@ import copy
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 from src.helperFunctions.readFromCSV import read_dataset
 
-goal = read_dataset('large')
-pop_count = 100
-generations = 50
+goal = read_dataset('small')
+pop_count = 1000
+generations = 100
 
 class belief_space(object):
     def __init__(self):
-        self.situational = None  
-        self.normative = {
-            'best_fitness': float('inf'),
-            'avg_fitness': 0,
-            'machine_load_distribution': {},
-            'job_affinity': {}  # Track which jobs perform well on which machines
-        }
+        self.situational = None
+        self.normative = None
     
     def update_situational(self, individual):
-        """Track the best solution found so far"""
-        if self.situational is None or individual.fitness < self.situational.fitness:
-            self.situational = copy.deepcopy(individual)
-            self.normative['best_fitness'] = individual.fitness
+        self.situational = copy.deepcopy(individual)
     
     def update_normative(self, population):
-        """Learn patterns from top performers"""
-        # Sort by fitness (lower is better)
-        sorted_pop = sorted(population, key=lambda x: x.fitness)
-        top_performers = sorted_pop[:max(1, len(sorted_pop) // 4)]  # Top 25%
-        
-        # Update average fitness
         fitness_values = [ind.fitness for ind in population]
-        self.normative['avg_fitness'] = sum(fitness_values) / len(fitness_values)
-        
-        # Learn machine load patterns from top performers
-        self.normative['machine_load_distribution'] = {}
-        for ind in top_performers:
-            for machine, tasks in ind.timeline.items():
-                total_load = sum(sum(task_dict.values()) for task_dict in tasks)
-                if machine not in self.normative['machine_load_distribution']:
-                    self.normative['machine_load_distribution'][machine] = []
-                self.normative['machine_load_distribution'][machine].append(total_load)
-        
-        # Calculate average load per machine from top performers
-        for machine in self.normative['machine_load_distribution']:
-            loads = self.normative['machine_load_distribution'][machine]
-            self.normative['machine_load_distribution'][machine] = sum(loads) / len(loads)
-        
-        # Learn job-to-machine affinity from top performers
-        self.normative['job_affinity'] = {}
-        for ind in top_performers:
-            for machine, tasks in ind.timeline.items():
-                for task_dict in tasks:
-                    for (job_id, task_id), exec_time in task_dict.items():
-                        if job_id not in self.normative['job_affinity']:
-                            self.normative['job_affinity'][job_id] = {}
-                        if machine not in self.normative['job_affinity'][job_id]:
-                            self.normative['job_affinity'][job_id][machine] = 0
-                        self.normative['job_affinity'][job_id][machine] += 1
+        avg_machine_time = 0
+        for ind in population:
+            timeline = ind.timeline
+            total_time = {k: 0 for k in timeline}  
 
-
+            for key in timeline:  
+                for task_dict in timeline[key]:
+                    total_machine_time , total_execution_time = list(timeline[key][-1].values())[0]
+                    total_time[key] = total_execution_time + total_machine_time
+            avg_machine_time += sum(total_time.values()) / len(total_time)
+        avg_machine_time /= len(population)
+        self.normative = {
+            'best_fitness': min(fitness_values),
+            'worst_fitness': max(fitness_values),
+            'avg_fitness': sum(fitness_values) / len(fitness_values),
+            'population_size': len(population),
+            'avg_machine_time':avg_machine_time
+        }
+    
+    def get_belief_influence(self):
+        return {
+            'best_solution': self.situational,
+            'normative_info': self.normative
+        }
 
 class individual(object):
     def __init__(self,timeline):
         self.timeline = timeline
         self.fitness = self.calc_fitness()
+
+
     @classmethod
     def initialize_individual(self):
         global goal
         clone = copy.deepcopy(goal)
         timeline = {}
+        
         for _ in range(clone['total_tasks']):
             job = random.randint(0,clone['total_jobs'] - 1)
             task = clone['jobs'][job]['tasks'].pop(0)
+            jobId = clone['jobs'][job]['job_id']
+
             if not clone['jobs'][job]['tasks']:
                 clone['jobs'].pop(job)
                 clone['total_jobs'] -= 1
+
+
+            wait_time = 0
             machine = random.randint(1,clone['machines_count'])
+
+
+            if task['task_id'] != 1:
+                last_task_start, last_task_exec = 0,0
+                for machine, lst in timeline.items():
+                    for d in lst:
+                        if (jobId,task['task_id'] - 1) in d:
+                            last_task_start, last_task_exec = d[(jobId,task['task_id'] - 1)]
+                            break
+                prev_start,prev_exec = list(timeline[machine][-1].values())[0]
+                wait_time = (last_task_start + last_task_exec) - (prev_start + prev_exec)
+                if (wait_time < 0): 
+                    wait_time = 0
+            
             if machine not in timeline:
                 timeline[machine] = []
-            timeline[machine].append({ (job,task['task_id']) : task['execution_time'] })   
+                timeline[machine].append({ (jobId,task['task_id']) : (0 + wait_time, task['execution_time'])  })   
+            else:
+                prev_start,prev_exec = list(timeline[machine][-1].values())[0]
+                timeline[machine].append({ (jobId,task['task_id']) : (prev_start + prev_exec + wait_time, task['execution_time'])  })
+        
         return individual(timeline)
     
     
@@ -91,60 +98,94 @@ class individual(object):
 
         for key in timeline:  
             for task_dict in timeline[key]:
-                for time in task_dict.values():
-                    total_time[key] += time
+                total_machine_time , total_execution_time = list(timeline[key][-1].values())[0]
+                total_time[key] = total_execution_time + total_machine_time
         fitness = max(total_time.values())
         for i in total_time:
             fitness += max(total_time.values()) - total_time[i]
         return fitness
-    
+
     def influence_from_belief_space(self, belief_space):
-        """Apply cultural influence to improve solution based on belief space"""
-        # Get learned patterns
-        job_affinity = belief_space.normative.get('job_affinity', {})
-        machine_loads = belief_space.normative.get('machine_load_distribution', {})
-        best_fitness = belief_space.normative.get('best_fitness', float('inf'))
-        
-        # Only apply influence if current individual is worse than best found
-        if self.fitness > best_fitness * 1.1:  # 10% worse than best
-            # Step 1: Identify overloaded machines
-            current_loads = {}
-            for machine, tasks in self.timeline.items():
-                current_loads[machine] = sum(sum(task_dict.values()) for task_dict in tasks)
+        """Apply influence from belief space to modify individual's solution"""
+        # Get belief influences
+        beliefs = belief_space.get_belief_influence()
+        best_solution = beliefs['best_solution']
+        normative_info = beliefs['normative_info']
+
+
+        if self.fitness > normative_info['avg_fitness']:
+            # If individual is worse than average, move toward best solution
+            influence_factor = random.uniform(0.1, 0.5)
             
-            # Step 2: Try to rebalance by moving tasks from overloaded to underloaded machines
-            sorted_machines = sorted(current_loads.items(), key=lambda x: x[1], reverse=True)
-            
-            if len(sorted_machines) > 1:
-                overloaded = sorted_machines[0][0]
-                underloaded = sorted_machines[-1][0]
-                
-                # Try moving a task from overloaded to underloaded
-                if overloaded in self.timeline and self.timeline[overloaded]:
-                    # Pick a task that prefers the underloaded machine based on affinity
-                    best_task_idx = 0
-                    if job_affinity:
-                        for idx, task_dict in enumerate(self.timeline[overloaded]):
-                            for (job_id, _), _ in task_dict.items():
-                                if job_id in job_affinity:
-                                    if underloaded in job_affinity[job_id]:
-                                        best_task_idx = idx
-                                        break
-                    
-                    # Move the task
-                    task_to_move = self.timeline[overloaded].pop(best_task_idx)
-                    if underloaded not in self.timeline:
-                        self.timeline[underloaded] = []
-                    self.timeline[underloaded].append(task_to_move)
-                    
-                    # Clean up empty machines
-                    if not self.timeline[overloaded]:
-                        del self.timeline[overloaded]
-                    
-                    # Recalculate fitness
-                    self.fitness = self.calc_fitness()
+            # Apply influence by mixing current solution with best solution
+            self.timeline = self._apply_influence(self.timeline, best_solution.timeline, influence_factor)
+            self.fitness = self.calc_fitness()
         
+        return self.fitness
     
+    def _apply_influence(self, current_timeline, best_timeline, factor):
+        
+        # Extract all tasks from current timeline with their execution times
+        all_tasks = []
+        for machine in current_timeline:
+            for task_dict in current_timeline[machine]:
+                for (jobId, task_id), (start_time, exec_time) in task_dict.items():
+                    all_tasks.append({
+                        'jobId': jobId,
+                        'task_id': task_id,
+                        'exec_time': exec_time,
+                        'current_machine': machine
+                    })
+        
+        # Sort tasks by job and task_id to process in order
+        all_tasks.sort(key=lambda x: (x['jobId'], x['task_id']))
+        
+        # Build new timeline respecting all constraints
+        influenced_timeline = {}
+        job_task_completion_times = {}  # Track when each (jobId, task_id) finishes
+        machine_end_times = {}  # Track when each machine becomes free
+        
+        for task_info in all_tasks:
+            jobId = task_info['jobId']
+            task_id = task_info['task_id']
+            exec_time = task_info['exec_time']
+            task_key = (jobId, task_id)
+            
+            # Probabilistically adopt machine from best solution
+            new_machine = task_info['current_machine']
+            if random.random() < factor:
+                # Find this task in best solution and use its machine
+                for machine in best_timeline:
+                    for task_dict in best_timeline[machine]:
+                        if task_key in task_dict:
+                            new_machine = machine
+                            break
+            
+            # Calculate earliest start time respecting constraints
+            earliest_start = 0
+            
+            # Constraint 1: Task must start after its prerequisite (task_id - 1) completes
+            if task_id > 1:
+                prerequisite_key = (jobId, task_id - 1)
+                if prerequisite_key in job_task_completion_times:
+                    earliest_start = max(earliest_start, job_task_completion_times[prerequisite_key])
+            
+            # Constraint 2: Task must start after machine is free
+            if new_machine in machine_end_times:
+                earliest_start = max(earliest_start, machine_end_times[new_machine])
+            
+            # Schedule task on the chosen machine
+            if new_machine not in influenced_timeline:
+                influenced_timeline[new_machine] = []
+            
+            start_time = earliest_start
+            end_time = start_time + exec_time
+            
+            influenced_timeline[new_machine].append({task_key: (start_time, exec_time)})
+            job_task_completion_times[task_key] = end_time
+            machine_end_times[new_machine] = end_time
+        
+        return influenced_timeline
 
 def main():
     population=[]
