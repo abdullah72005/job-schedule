b69a6d504b205a55d8e5cc664848ca2632152da9
marwan-salem18 <marwan.salem209@gmail.com>
2025-11-29 14:29:36 +0200

    a checkpoint

--- src/cultural/cultural.py
+++ src/cultural/cultural.py
@@ -5,7 +5,7 @@ import copy
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 from src.helperFunctions.readFromCSV import read_dataset
 
-goal = read_dataset('large')
+goal = read_dataset('small')
 pop_count = 100
 generations = 50
 
@@ -72,16 +72,22 @@ class individual(object):
         global goal
         clone = copy.deepcopy(goal)
         timeline = {}
+        
         for _ in range(clone['total_tasks']):
             job = random.randint(0,clone['total_jobs'] - 1)
             task = clone['jobs'][job]['tasks'].pop(0)
+            jobId = clone['jobs'][job]['job_id']
             if not clone['jobs'][job]['tasks']:
                 clone['jobs'].pop(job)
                 clone['total_jobs'] -= 1
+            
             machine = random.randint(1,clone['machines_count'])
             if machine not in timeline:
                 timeline[machine] = []
-            timeline[machine].append({ (job,task['task_id']) : task['execution_time'] })   
+                timeline[machine].append({ (jobId,task['task_id']) : ( task['execution_time'])  })   
+            else:
+                timeline[machine].append({ (jobId,task['task_id']) : (task['execution_time'])  })
+        
         return individual(timeline)
     
     
@@ -98,8 +104,52 @@ class individual(object):
             fitness += max(total_time.values()) - total_time[i]
         return fitness
     
+    def _can_move_task(self, job_id, task_id, source_machine, target_machine):
+        """
+        Check if a task can be moved while respecting job task ordering.
+        A task can only move if all previous tasks of the same job are not on a later machine.
+        """
+        # Find all tasks of this job and their positions
+        job_task_positions = {}
+        
+        for machine, tasks in self.timeline.items():
+            for task_dict in tasks:
+                for (jid, tid), _ in task_dict.items():
+                    if jid == job_id:
+                        if jid not in job_task_positions:
+                            job_task_positions[jid] = []
+                        job_task_positions[jid].append((tid, machine))
+        
+        # Get all task IDs for this job, sorted to infer ordering
+        task_ids = sorted([tid for tid, _ in job_task_positions.get(job_id, [])])
+        
+        if not task_ids:
+            return True
+        
+        current_idx = task_ids.index(task_id) if task_id in task_ids else -1
+        if current_idx == -1:
+            return True
+        
+        # Check: all previous tasks (lower task_id) must be either:
+        # 1. On the same target machine, or
+        # 2. Not yet scheduled (shouldn't happen in valid state)
+        for prev_task_id in task_ids[:current_idx]:
+            prev_machines = [m for tid, m in job_task_positions.get(job_id, []) if tid == prev_task_id]
+            # Previous task should exist; if it does, it's acceptable if it's on target or same machine
+            if prev_machines and target_machine not in prev_machines and source_machine not in prev_machines:
+                return False
+        
+        # Check: all subsequent tasks (higher task_id) must stay on same or later machines
+        for next_task_id in task_ids[current_idx + 1:]:
+            next_machines = [m for tid, m in job_task_positions.get(job_id, []) if tid == next_task_id]
+            # If next task is already scheduled, it shouldn't be on an earlier machine
+            if next_machines and next_machines[0] < target_machine:
+                return False
+        
+        return True
+
     def influence_from_belief_space(self, belief_space):
-        """Apply cultural influence to improve solution based on belief space"""
+        """Apply cultural influence to improve solution based on belief space while respecting job ordering"""
         # Get learned patterns
         job_affinity = belief_space.normative.get('job_affinity', {})
         machine_loads = belief_space.normative.get('machine_load_distribution', {})
@@ -121,28 +171,32 @@ class individual(object):
                 
                 # Try moving a task from overloaded to underloaded
                 if overloaded in self.timeline and self.timeline[overloaded]:
-                    # Pick a task that prefers the underloaded machine based on affinity
-                    best_task_idx = 0
-                    if job_affinity:
-                        for idx, task_dict in enumerate(self.timeline[overloaded]):
-                            for (job_id, _), _ in task_dict.items():
-                                if job_id in job_affinity:
+                    # Pick a task that prefers the underloaded machine and respects ordering
+                    best_task_idx = -1
+                    
+                    for idx, task_dict in enumerate(self.timeline[overloaded]):
+                        for (job_id, task_id), _ in task_dict.items():
+                            # Check if this task can be moved while respecting job ordering
+                            if self._can_move_task(job_id, task_id, overloaded, underloaded):
+                                best_task_idx = idx
+                                # Prefer tasks with affinity to target machine
+                                if job_affinity and job_id in job_affinity:
                                     if underloaded in job_affinity[job_id]:
-                                        best_task_idx = idx
                                         break
                     
-                    # Move the task
-                    task_to_move = self.timeline[overloaded].pop(best_task_idx)
-                    if underloaded not in self.timeline:
-                        self.timeline[underloaded] = []
-                    self.timeline[underloaded].append(task_to_move)
-                    
-                    # Clean up empty machines
-                    if not self.timeline[overloaded]:
-                        del self.timeline[overloaded]
-                    
-                    # Recalculate fitness
-                    self.fitness = self.calc_fitness()
+                    # Only move if we found a valid task
+                    if best_task_idx >= 0:
+                        task_to_move = self.timeline[overloaded].pop(best_task_idx)
+                        if underloaded not in self.timeline:
+                            self.timeline[underloaded] = []
+                        self.timeline[underloaded].append(task_to_move)
+                        
+                        # Clean up empty machines
+                        if not self.timeline[overloaded]:
+                            del self.timeline[overloaded]
+                        
+                        # Recalculate fitness
+                        self.fitness = self.calc_fitness()
         
     
 
@@ -153,6 +207,7 @@ def main():
 
     belief = belief_space()
     belief.update_situational(min(population, key=lambda ind: ind.fitness))
+    print(belief.situational.timeline)
     belief.update_normative(population)
     for i in range(generations):
         for ind in population:
